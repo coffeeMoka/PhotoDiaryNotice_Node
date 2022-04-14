a948962ad5d7aa25cf021b422d92eaa58f4f40ec
Coffee Fuyunatsu <coffeefuyunatsu@gmail.com>
2022-04-14 23:36:55 +0900

    update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
+import * as puppeteer from "puppeteer";
 import Cast from "./models/cast"
 import { serv } from "./server";
 
 console.log("Hello, world");
 serv();
 console.log(Cast.name);
+
+const launchOptions: puppeteer.BrowserLaunchArgumentOptions = {
+    headless: false,
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox"
+    ]
+};
+
+const waitOptions: puppeteer.WaitForOptions = {
+    waitUntil: ["domcontentloaded", "networkidle0"]
+};
+
+(async () => {
+    const browser: puppeteer.Browser = await puppeteer.launch(launchOptions);
+    const page: puppeteer.Page = await browser.newPage();
+    await page.goto("https://www.google.com/", waitOptions);
+
+    await browser.close();
+    console.log("end");
+})();
